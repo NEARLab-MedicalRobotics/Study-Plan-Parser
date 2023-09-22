53d087faef7669ada91ef7670a239cbd9a7d998b
Alberto Rota <alberto_rota@outlook.com>
2023-09-22 15:29:08 +0200

    22 09

---
+++ piani_di_studio.py
@@ -0,0 +1,69 @@
+# %%
+import camelot
+import os
+from PyPDF2 import PdfReader
+import pandas as pd
+
+
+# %%
+folder = "split"
+allnames = []
+alreadyparsed = os.listdir("output")
+fileerrors = []
+print(f"> Found {len(os.listdir(folder))} files")
+for nf,file in enumerate(os.listdir(folder)):
+    print(f"> Processing file {nf+1}/{len(os.listdir(folder))}: ",end="")
+    file = os.path.join(folder,file)
+    table = camelot.read_pdf(file)
+    try:
+        table = table[0].df
+    except:
+        print(f">> Error on {file}")
+        fileerrors.append(file)
+        continue
+    reader = PdfReader(file)
+    page = reader.pages[0]  
+    text = page.extract_text()
+    header = text.split("AA AC Sem Pos Codice Insegnamento Cfu Tipo Tipo")[0]
+    media = text.split("Ripete")[-1]
+    header.replace(": ", ": \t")
+    header = header.split("\n ")[-1]
+    header.replace("Nome:", "\n\nNome:")
+    try:
+        name = header.split("Nome: ")[1].split("Matricola:")[0].strip().replace("\nCognome: ", "_").replace(" ", "")
+        print(name, end="")
+        if name+".txt" in alreadyparsed:
+            print("--> ALREADY PARSED")
+            continue 
+        else:
+            print()
+    except:
+        print(f">> Cannot find name on {file}")
+        fileerrors.append(file)
+        continue
+    
+    print(f"> Processing: {name}")
+    if name in allnames:
+        print("DUPLICATE")
+    allnames.append(name)
+    
+    matricola = header.split("Matricola: ")[1].split("Data")[0].strip()
+
+    t = table[[0,1,2,3,6,4,5,12,13,14,15,16,17]].copy()
+    t[5] = [i.replace("\n"," ") for i in t[5]]
+    # t[5] = [i.replace('"', "") for i in t[5]]
+    for idx,cfu in enumerate(t[6]):
+        if len(cfu) == 1:
+            cfu = "0" + cfu
+            t[6][idx] = cfu
+    # t[6] = [f" {int(cfu) : 02d} " for cfu in t[6]]
+    tablestring = t.to_csv(sep = " ", index = False, header = False, path_or_buf=None)
+    tablestring= tablestring.replace('"', "")
+
+    final = header + "\n" + tablestring + media
+    with open(os.path.join("output",name+".txt"), "w") as f:
+        f.write(final)
+        
+with open("errors.txt", "w") as f:
+    f.write(str(fileerrors))
+
